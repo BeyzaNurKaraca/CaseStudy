41c7a0c56e8af6868a23dcf4b38527aa1fc183e7
Beyza <beyzanurkraca@gmail.com>
2023-01-25 22:38:56 +0300

    CaseStudy

---
+++ SanlabCase/SanlabCase.Client/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using System.Net.Sockets;
+using System.Text;
+
+namespace SanlabCase.Client
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                TcpClient client=new TcpClient("127.0.0.1",1881);
+                //string messageToSend = "Hello";
+                Console.WriteLine("Mesajınızı giriniz: ");
+                string messageToSend = Console.ReadLine();
+              
+                int byteCount=Encoding.ASCII.GetByteCount(messageToSend+1);
+                byte[] sendData=Encoding.ASCII.GetBytes(messageToSend);
+
+                NetworkStream stream=client.GetStream();
+                stream.Write(sendData, 0, sendData.Length);
+                Console.WriteLine("Sunucuya veri gönderiliyor...");
+
+                StreamReader sr=new StreamReader(stream);
+                string response = sr.ReadLine();
+                Console.WriteLine(response);
+
+                stream.Close();
+                client.Close();
+                Console.ReadKey();
+
+
+            }
+            catch (Exception e)
+            {
+
+                Console.WriteLine("Bağlantı hatası.");
+            }
+        }
+    }
+}
---
+++ SanlabCase/SanlabCase.Server/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace SanlabCase.Server
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            TcpListener listener = new TcpListener(IPAddress.Any,1881);
+            listener.Start();
+            while (true)
+            {
+                TcpClient client = listener.AcceptTcpClient();
+                Console.WriteLine("Client bağlandı.");
+                NetworkStream stream = client.GetStream();
+                StreamReader sr=new StreamReader(client.GetStream());
+                StreamWriter sw=new StreamWriter(client.GetStream());
+
+                try
+                {
+                    byte[] buffer = new byte[1024];
+                    stream.Read(buffer, 0, buffer.Length);
+                    int recv = 0;
+                    foreach (byte b in buffer)
+                    {
+                        if (b!=0)
+                        {
+                            recv++;
+                        }
+                    }
+                    string request=Encoding.UTF8.GetString(buffer,0,recv);
+                    Console.WriteLine("İstek alındı: "+request);
+                    sw.WriteLine($"Client mesajı: {request} {DateTime.Now}");
+                    sw.Flush();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Hata oluştu.");
+                    sw.WriteLine(e.ToString());
+                }
+
+            }
+
+        }
+    }
+}
